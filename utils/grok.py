972524899a7df2d85f0cb67907d9ded2eedbcf94
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-09-14 08:08:53 +0300

    more gemini models

--- utils/grok.py
+++ utils/grok.py
@@ -12,7 +12,7 @@ class Grok():
             base_url = self.endpoint,
         )
         
-        self.models = ["xai/grok-3-mini", "xai/grok-3"] #, "openai/gpt-4.1-nano", "openai/gpt-4.1-mini", "openai/gpt-4.1"] #, "openai/gpt-4o-mini", "openai/gpt-4o"]
+        self.models = ["xai/grok-3-mini", "xai/grok-3", "openai/gpt-4.1-nano", "openai/gpt-4.1-mini", "openai/gpt-4.1"] #, "openai/gpt-4o-mini", "openai/gpt-4o"]
         
     def get_response(self, query):  
         if self.models:      
