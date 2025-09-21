0af3e63ad2e9c980f4a2cbb8550f7f8e5d5e5f98
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-09-21 05:05:33 +0300

    Update auto_bet_red.py

--- auto_bet_red.py
+++ auto_bet_red.py
@@ -126,7 +126,7 @@ class AutobetRed:
                 slips_over.append(betslip_over)
                 
         composite_betslips_red = self.get_composite_betslips(slips_red, 6)         
-        composite_betslips_over = self.get_composite_betslips(slips_over, 4)
+        composite_betslips_over = self.get_composite_betslips(slips_over, 5)
         
         composite_betslips = composite_betslips_over #+ composite_betslips_red
         
