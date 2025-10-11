a746b7182f8d2d3e518a8202d418d8a6d01418e0
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-10-11 07:55:13 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -109,7 +109,7 @@ class Predict:
         return query
     
     def is_valid_match(self, filtered_match):
-        MIN_ODD, MAX_ODD, MIN_PROB = 1.15, 1.79, 70
+        MIN_ODD, MAX_ODD, MIN_PROB = 1.15, 1.79, 75
         
         filtered_match = (
             filtered_match
