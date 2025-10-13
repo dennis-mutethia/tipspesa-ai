a079769008f7eaadec2aa070cfdc1febfe50fcee
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-10-13 19:32:21 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -122,7 +122,8 @@ class Predict:
         if filtered_match:
             filtered_match = (
                 None 
-                if (int(filtered_match['sub_type_id']) == 1  and int(filtered_match['outcome_id']) == 1 and filtered_match['odd'] >= 1.45) 
+                if (int(filtered_match['sub_type_id']) == 1  and int(filtered_match['outcome_id']) == 1 and filtered_match['odd'] >= 1.45)
+                or (int(filtered_match['sub_type_id']) == 1  and int(filtered_match['outcome_id']) == 3 and filtered_match['odd'] <= 1.30) 
                 or (filtered_match["bet_pick"].lower() == 'over 1.5' and filtered_match['odd'] >= 1.27)
                 or (int(filtered_match['sub_type_id']) == 10 and filtered_match["odd"] >= 1.2)
                 #or (int(filtered_match['sub_type_id']) == 29 and filtered_match["odd"] >= 1.3)
