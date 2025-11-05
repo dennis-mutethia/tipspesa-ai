b31ab213f03fedee64860c563bec3b2e66a318bb
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-11-05 11:14:03 +0300

    chedk if is_market_available

--- tasks/autobet.py
+++ tasks/autobet.py
@@ -2,6 +2,7 @@
 import concurrent.futures
 import logging
 
+from utils.betika import Betika
 from utils.helper import Helper
 from utils.db import Db
 
@@ -13,19 +14,46 @@ class Autobet:
         main class
     """
     def __init__(self):
+        self.betika = Betika()
         self.db = Db()
     
+    def is_market_available(self, match):
+        try:
+            url = f'https://api.betika.com/v1/uo/match?parent_match_id={match.get("parent_match_id")}'
+            match_details = self.betika.get_data(url)
+            if not match_details:
+                return None     
+            
+            for datum in match_details.get('data', []):
+                if int(datum.get('sub_type_id')) == match.get("sub_type_id"):
+                    for odd in datum.get('odds', []):
+                        if odd.get('odd_key') ==  match.get("bet_pick"):
+                            match["odd"] = odd.get('odd_value')
+                            return match
+                        
+        except Exception as e:
+            logger.error(e)
+            
+        return None
+        
+    
     def bet(self, profile):
-        phone = profile[0]
-        password = profile[1]
-        profile_id = profile[2]
-        matches = self.db.fetch_unplaced_matches(profile_id)
-        helper = Helper(phone, password)
-        #helper.auto_bet(profile_id, matches, 1)
-        #helper.auto_bet(profile_id, matches, 2)
-        #helper.auto_bet(profile_id, matches, 3)
-        helper.auto_bet(profile_id, matches, 4)
-        #helper.auto_bet(profile_id, matches, 5)
+        try:
+            phone = profile[0]
+            password = profile[1]
+            profile_id = profile[2]
+            unplaced_matches = self.db.fetch_unplaced_matches(profile_id)
+            
+            matches = []                    
+            for match in unplaced_matches:
+                match = self.is_market_available(match)
+                if match:
+                    matches.append(match)
+                                
+            helper = Helper(phone, password)
+            helper.auto_bet(profile_id, matches, 4)
+        except Exception as e:
+            logger.error(e)
                     
     def __call__(self):
         # Use ThreadPoolExecutor to spawn a thread for each profile
