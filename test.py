0e457115429906ac246c7e6b808411e74c0094b2
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-11-05 11:23:37 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+
+
+import logging
+from tasks.autobet import Autobet
+from tasks.predict import Predict
+
+
+# Global logging configuration (applies to all modules)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'  # Optional: Custom date format (e.g., 2025-11-04 22:13:45)
+)
+
+Autobet()()
