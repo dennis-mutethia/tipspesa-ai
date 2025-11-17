4ce3354802cad1dbdde52bf43ad97feda9ff9428
Dennis Mutethia Muga <dennismuga@gmail.com>
2025-11-17 11:25:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         func=predict_dropping_task,
         trigger=CronTrigger(
             hour="*",      # Every hour            
-            minute="*/10",   #Every 10 mins
+            minute="0", 
             second="0"
         ),
         id="predict_dropping_cron",
